a3180736cb73e735aafa00889ec73e59b698986b
G G <FedeOne02@outlook.it>
2018-12-29 17:27:23 +0100

    Ho  commentato dalla riga 12 alla riga 15.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,11 +8,14 @@ namespace ConsoleApp1
         {
             int a = int.MaxValue;
             Console.WriteLine(a); //"int" è un tipo di variabile che ricopre un raggio determinato di numeri interi. ".MaxValue" è servito per mostrare quale è il valore massimo;
-            Console.WriteLine(a + 1); //Valore massimo del tipo "int" +1, cioè di un'unità più grande di quanto possa contenere;
-            int b = -128;
-            int c = -b;
-            Console.WriteLine(Convert.ToString(b, 2));
-            Console.WriteLine(Convert.ToString(c, 2));
+            if (a+1<=int.MaxValue)
+            {
+                Console.WriteLine(a + 1); //Valore massimo del tipo "int" +1, cioè di un'unità più grande di quanto possa contenere;
+            }
+            int b = -128; //Inizializzo la variabile "b" che ci servirà per la verifica di un numero binario dopo averne ricavato l'opposto;
+            int c = -b; //Inizializzo la variabile "c" che sarà l'opposto del valore della variabile "b";
+            Console.WriteLine(Convert.ToString(b, 2)); //Stampiamo la variabile "b" in base 2, il cui valore intero in base 10 è -128;
+            Console.WriteLine(Convert.ToString(c, 2)); //Stampiamo la variabile "c" in base 2, il cui valore intero in base 10 è 128;
             int value = 31;
             int somma = value;
             value = ~value + 1; //NOT;
